f00f2b9f61cb2e92a8226146d7946fb38ea0bba3
Emilio David Clemente Silmi <emiliodavidcs@gmail.com>
2020-03-30 08:42:28 +0200

    Vue instance bootstrap file

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import pinDirective from "./shared/pin-directive";
+import currencyFilter from "./shared/currency-filter";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.directive("pin", pinDirective);
+Vue.filter("currency", currencyFilter);
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+  router,
+  store
+}).$mount("#app");

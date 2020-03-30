5e638aa925e9c779b645dc75b2b85b70b537c3f5
Emilio David Clemente Silmi <emiliodavidcs@gmail.com>
2020-03-30 08:41:41 +0200

    Cart component

---
+++ src/components/cart/ShoppingCart.vue
@@ -0,0 +1,86 @@
+<template>
+  <div>
+    <h1>Cart</h1>
+    <table>
+      <thead>
+        <tr>
+          <th class="robot-title">
+            Robot
+          </th>
+          <th class="cost">
+            Cost
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(robot, index) in cart" :key="index">
+          <td class="robot-title">
+            {{ robot.head.title }}
+          </td>
+          <td class="cost">
+            {{ robot.cost | currency("€") }}
+          </td>
+        </tr>
+      </tbody>
+    </table>
+    <h2 class="sale-items">You saved money on these robots</h2>
+    <table>
+      <thead>
+        <tr>
+          <th class="robot-title">
+            Robot
+          </th>
+          <th class="cost">
+            Cost
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(robot, index) in cartSaleItems" :key="index">
+          <td class="robot-title">
+            {{ robot.head.title }}
+          </td>
+          <td class="cost">
+            {{ robot.cost | currency("€") }}
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+// import currencyFilter from "../../shared/currency-filter";
+import { mapState, mapGetters } from "vuex";
+
+export default {
+  name: "Cart",
+  // // to declare filters locally
+  // filters: {
+  //   currency: currencyFilter
+  // },
+  computed: {
+    ...mapState({ cart: state => state.robots.cart }),
+    ...mapGetters("robots", ["cartSaleItems"])
+  }
+};
+</script>
+
+<style scoped>
+td,
+th {
+  padding: 5px;
+}
+.robot-title {
+  text-align: left;
+  padding-right: 200px;
+}
+.cost {
+  text-align: right;
+}
+.sale-items {
+  margin-top: 50px;
+  font-size: 18px;
+  color: red;
+}
+</style>
